1c6fa38d91992fafef482d8d12f8d474cf3abe08
James-Adam Renquinha Henri <jarhmander@gmail.com>
2014-04-13 02:48:32 -0400

    New OAM segment and OAM array var.
    TODO: after just adding the "ready video" var, we could now write a tiny part
    of PPU_updates().

--- src/video.s
+++ src/video.s
@@ -3,6 +3,11 @@
 .include "core.inc"
 ;-------------------------------------------------------------------------------
 
+    .constructor video_ctor
+
+    .segment "OAM"
+OAM: .res 256
+
     .code
 
 ;-------------------------------------------------------------------------------
@@ -14,7 +19,17 @@
 ; - while data in PPU_buff: interpret data, update VRAM address, set inc, write.
 ;  (It could be a bit more specialised, like update palette, nametable and 
 ; attributes separately)
-
+; - reset scroll (!)
     rts
 .endproc
 ;-------------------------------------------------------------------------------
+.proc video_ctor
+    movw r0, #OAM
+    movw r2, #$FF
+    movw r4, #256
+    jsr memset
+    mov $2003, #0
+    rts
+.endproc
+;-------------------------------------------------------------------------------
+
